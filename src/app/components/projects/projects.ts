a6554e06ab6ac9ba65b8fcada447d24fa5ac53dd
David Pardo <dajupaco@gmail.com>
2025-09-11 21:35:51 +0200

    feat: timelie broken

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NgxTimelineComponent, TimelineEntry } from '@omnedia/ngx-timeline';
+import { NgxWordPullupComponent } from '@omnedia/ngx-word-pullup';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NgxWordPullupComponent, NgxTimelineComponent],
   templateUrl: './projects.html',
-  styleUrl: './projects.css'
+  styleUrl: './projects.css',
 })
 export class Projects {
-
+  timelineData = [
+    {
+      title: '<p>Start</p>',
+      content: '<div>The beginning of the project.<div>',
+    },
+    {
+      title: '<p>Development</p>',
+      content: 'Key development milestones achieved.',
+    },
+    {
+      title: '<p>Launch</p>',
+      content: '<div>The project goes live!</div>',
+    },
+  ] as TimelineEntry[];
 }
