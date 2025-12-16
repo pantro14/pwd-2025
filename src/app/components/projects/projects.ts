b4210c1f790dc17a1f7e57fe91f771b8940421e0
David Pardo <dajupaco@gmail.com>
2025-12-16 17:16:10 +0100

    feat: updated deps and fix styling issues

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,38 +1,14 @@
-import { Component, computed, signal } from '@angular/core';
-import { NgxTimelineComponent } from '@omnedia/ngx-timeline';
+import { Component, signal } from '@angular/core';
+import { NgxTimelineComponent, NgxTimelineEntryComponent } from '@omnedia/ngx-timeline';
 import { NgxWordPullupComponent } from '@omnedia/ngx-word-pullup';
 import { Project, projects } from '../../config/page-config';
 
 @Component({
   selector: 'app-projects',
-  imports: [NgxWordPullupComponent, NgxTimelineComponent],
+  imports: [NgxWordPullupComponent, NgxTimelineComponent, NgxTimelineEntryComponent],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
   protected readonly projects = signal<Project[]>([...projects]);
-
-  protected readonly projectsTimeline = computed(() => {
-    return this.projects().map((project) => ({
-      title: `<div class="flex flex-row items-center space-x-4 py-3">
-                <p class="text-3xl">${project.year}</p>
-                <img src="${project.image}" class="w-30 h-30" />
-              </div>`,
-      content: `<div class="flex flex-col py-3 space-y-2">
-  <div class="flex-1">
-    <div class="flex items-center space-x-2">
-      <a class="font-bold md:text-xl underline" target="_blank" href="${project.link}">
-        ${project.name}
-      </a>
-      ${project.countries
-        .map((country) => `<img src="projects/${country}.svg" class="w-6 h-6" />`)
-        .join('')}
-    </div>
-  </div>
-
-  <p class="font-light md:text-lg text-rose-300">${project.position}</p>
-  <p class="font-light text-gray-400 text-justify">${project.description}</p>
-</div>`,
-    }));
-  });
 }
